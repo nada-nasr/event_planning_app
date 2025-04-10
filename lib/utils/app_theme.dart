ef890a60e7d1cdc2a43dcd2464ef6afa88f88fdf
Nada Nasr <nnasr7523@gmail.com>
2025-04-10 13:58:06 +0200

    + Added profile tab in home screen

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,7 +1,18 @@
+import 'package:event_planning_app/utils/app_colors.dart';
+import 'package:event_planning_app/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final ThemeData lightTheme = ThemeData();
+  static final ThemeData lightTheme = ThemeData(
+      textTheme: TextTheme(
+          headlineLarge: AppStyles.bold20Black
+      )
+  );
 
-  static final ThemeData darkTheme = ThemeData();
+  static final ThemeData darkTheme = ThemeData(
+      scaffoldBackgroundColor: AppColors.primaryDark,
+      textTheme: TextTheme(
+          headlineLarge: AppStyles.bold20white
+      )
+  );
 }
